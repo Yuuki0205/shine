4faa090839eb106cadf7c8dcd7524286c765c93a
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-27 19:29:55 +0900

    編集　削除の実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,8 @@
 class TextsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_text, only: [:show]
+  before_action :set_text, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
   def index
     @texts = Text.all.order('created_at DESC')
   end
@@ -19,6 +21,20 @@ class TextsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+  
+  def update
+    if @text.update(text_params)
+      redirect_to text_path
+    else
+      render :edit
+  end
+  def destory
+    redirect_to root_path if @text.destroy
+  end
+
+
 
   private
   def text_params
@@ -28,4 +44,9 @@ class TextsController < ApplicationController
   def set_text
     @text = Text.find(params[:id])
   end
+  def move_to_index
+    redirect_to root_path if current_user.id != @text.user_id 
+   
+  end
+ end
 end
