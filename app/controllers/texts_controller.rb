ab62869a3f4a7e8a1231ab497dd0c9ed34f821fd
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-27 18:17:04 +0900

    text詳細ページの実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,8 @@
 class TextsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_text, only: [:show]
   def index
+    @texts = Text.all.order('created_at DESC')
   end
   def new
     @text = Text.new
@@ -14,9 +16,16 @@ class TextsController < ApplicationController
         render :new
       end
   end
+  def show
+  end
+
 
   private
   def text_params
     params.require(:text).permit(:one, :two, :three, :four, :five, :six, :seven, :eight, :nine, :ten).merge(user_id: current_user.id)
+  end
+
+  def set_text
+    @text = Text.find(params[:id])
   end
 end
